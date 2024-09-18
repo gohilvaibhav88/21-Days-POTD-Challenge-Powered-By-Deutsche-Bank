d47dec293c49869ce745967bcb85b496acddbf62
Vaibhav Gohil <114984034+gohilvaibhav88@users.noreply.github.com>
2024-09-18 16:44:23 +0530

    seventh  commit

---
+++ Day7.java
@@ -0,0 +1,34 @@
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        
+        Stack<Character> st = new Stack<>();
+        
+        
+        for(int i =0 ; i<x.length() ; i++){
+            
+            char current = x.charAt(i);
+            
+            if((!st.isEmpty() && current ==']') && st.peek() == '['){
+                st.pop();
+            }
+            else if((!st.isEmpty() && current =='}') && st.peek() == '{'){
+                st.pop();
+            }
+            else if((!st.isEmpty() && current ==')') && st.peek() == '('){
+                st.pop();
+            }else{
+                st.push(current);
+            }
+            
+            
+        }
+        
+        
+        
+        
+        return st.isEmpty();
+    }
+}
