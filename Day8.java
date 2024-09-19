dd4b5745b0d7cb00383470a4c37d05ab82fe9b36
Vaibhav Gohil <114984034+gohilvaibhav88@users.noreply.github.com>
2024-09-19 22:12:22 +0530

    eight  commit

---
+++ Day8.java
@@ -0,0 +1,37 @@
+class Solution {
+    // Function to reverse words in a given string.
+    String reverseWords(String str) {
+        String arr[] = str.split("\\.");
+        
+        int i =0 ;
+        int j = arr.length-1;
+        
+        
+        while(i < j){
+            String temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            
+            i++;
+            j--;
+        }
+        
+        StringBuilder newstr = new StringBuilder();
+        
+        
+        for(int k=0 ; k<arr.length ; k++){
+            newstr.append(arr[k]);
+            
+            if(k != arr.length-1){
+                
+                newstr.append('.');
+            }
+            
+            
+        }
+        
+        return newstr.toString();
+        
+        
+    }
+}
