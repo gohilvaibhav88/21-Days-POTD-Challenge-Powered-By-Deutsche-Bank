178d6ef2089e5d53d34ebd4a995aa54fe3165858
Vaibhav Gohil <114984034+gohilvaibhav88@users.noreply.github.com>
2024-09-20 08:21:13 +0530

    nine  commit

---
+++ Day9.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Returns count buildings that can see sunlight
+    public int countBuildings(int[] height) {
+        int max = height[0];
+        int count =1;
+        
+        
+        for(int i =1 ; i<height.length ; i++){
+            if(height[i] > max){
+                max = height[i];
+                count++;
+            }
+        }
+        
+        
+        return count;
+    }
+}
