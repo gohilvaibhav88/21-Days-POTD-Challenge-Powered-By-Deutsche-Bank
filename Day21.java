26cb02394f12ea8ef02691692d2ce47ccc4ac1b6
Vaibhav Gohil <114984034+gohilvaibhav88@users.noreply.github.com>
2024-10-03 00:41:58 +0530

    seventeenth commit

---
+++ Day21.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int rotateDelete(ArrayList<Integer> arr) {
+        
+       int n = arr.size();
+       int temp = n/2;
+       
+       int i = 3 * (temp/2);
+       
+       if(temp%2== 1){
+           i++;
+       }
+       
+       return arr.get(n-i);
+    }
+}
